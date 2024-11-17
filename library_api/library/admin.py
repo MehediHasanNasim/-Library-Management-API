4779cb1814a2fe3de0b62e707f6be6b870bcd796
MehediHasanNasim <75909031+MehediHasanNasim@users.noreply.github.com>
2024-11-17 07:00:32 +0600

    Customize admin section

--- library_api/library/admin.py
+++ library_api/library/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+from django.contrib.auth.admin import UserAdmin
+
+
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    # Optionally, customize the admin panel for the User model
+    list_display = ('username', 'email', 'role', 'is_staff', 'is_active')
+    list_filter = ('role', 'is_staff', 'is_active')
+    search_fields = ('username', 'email')
+    ordering = ('username',)
+
+
+    # Fields to show on the user creation and edit pages
+    fieldsets = UserAdmin.fieldsets + (
+        ('Custom Fields', {'fields': ('role',)}),  # Add your custom field here
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        ('Custom Fields', {'fields': ('role',)}),  # Ensure the role field is in the creation form
+    )
+    
+    
+admin.site.register(Book) 
+admin.site.register(BorrowRecord) 
+admin.site.register(BookReturn) 
+admin.site.register(Fine) 
+admin.site.register(Submission) 
+
